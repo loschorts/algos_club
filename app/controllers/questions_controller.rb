1aa1e35a185b362cd63f89f9e5509fb539a1a746
Ken Tran <loschorts@gmail.com>
2016-11-21 14:25:59 -0800

    fix greeting bug

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index]
 
   # GET /questions
   # GET /questions.json
